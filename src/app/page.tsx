9551ecac89e47b542eebc00687ffb425c7e3573b
Abdur Rahman <aburahman918@gmail.com>
2025-01-05 10:35:35 +0000

    Update 2.1

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// src/app/page.tsx
+import EnhancedPressureMonitor from '@/components/EnhancedPressureMonitor';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <EnhancedPressureMonitor />
+    </main>
+  );
+}
